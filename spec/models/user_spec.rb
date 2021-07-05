f39aa6ae27b2d08ec561c7571e8aff216f8c384e
Ikemoto-Yu <cosper79@gmail.com>
2021-07-05 15:41:19 +0900

    修正1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,17 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
    describe "ユーザー新規登録" do
+
+    context 'ユーザー登録できる時' do
+      it 'パスワードが一致すれば登録できる,パスワードが6文字以上の英数字混合であれば登録できる' do
+      @user.password = 'ikemotoyu7'
+      @user.password_confirmation = 'ikemotoyu7'
+      expect(@user).to be_valid
+      end
+    end
+
+    context 'ユーザー登録できない時' do
+
     it "nicknameが空だと登録できない"do
     @user.nickname = ''
     @user.valid?
@@ -111,5 +122,18 @@ RSpec.describe User, type: :model do
     @user.valid?
     expect(@user.errors.full_messages).to include "Birthday can't be blank"
     end
+    it 'emailは@がないと登録できない' do
+      @user.email = 'testtest1'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email is invalid"
+    end
+    it 'emailは一意性である' do
+    @user.save
+    another_user = FactoryBot.build(:user)
+    another_user.email = @user.email
+    another_user.valid?
+    expect(another_user.errors.full_messages).to include "Email has already been taken"
+    end
+   end
    end
 end
