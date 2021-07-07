20db788034271b491e280da7b41d012e24a6cc9b
Ikemoto-Yu <cosper79@gmail.com>
2021-07-07 17:41:56 +0900

    delivery_localの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,12 +7,14 @@ class Item < ApplicationRecord
   validates :category_id , numericality: { other_than: 1, message: "can't be blank" } 
   validates :appearance_id , numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_date_id , numericality: { other_than: 1, message: "can't be blank" } 
-  validates :delivery_local_id
-  validates :delivery_style_id
+  validates :delivery_local_id , numericality: { other_than: 0, message: "can't be blank" } 
+  validates :delivery_style_id , numericality: { other_than: 1, message: "can't be blank" } 
   validates :price
   end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :appearance
   belongs_to :delivery_date
+  belongs_to :delivery_style
+  belongs_to :delivery_local
 end
