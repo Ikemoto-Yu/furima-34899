c5e1d6b06553678486a56005ad84cc39d92873e2
Ikemoto-Yu <cosper79@gmail.com>
2021-07-07 15:30:09 +0900

    activehash実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,14 @@
 class ItemsController < ApplicationController
 
   def index
-    # @item = Item.new
+     @item = Item.new
+  end
+
+  def new
+    @item = Item.new
+  end
+  private
+  def item_params
+    params.require(:item).permit(:name,:info,:category_id,:appearance_id,:delivery_date_id,:delivery_local_id,:delivery_style_id,:price).merge(user_id: current.user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+  with_options presence: true do
+  validates :name
+  validates :info
+  validates :category_id
+  validates :appearance_id
+  validates :delivery_date_id
+  validates :delivery_local_id
+  validates :delivery_style_id
+  validates :price
+  end
 end
