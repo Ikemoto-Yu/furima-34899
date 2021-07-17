b3a825102f4fa9f8bd4f3da9e896aa8e23b2f64d
Ikemoto-Yu <cosper79@gmail.com>
2021-07-17 10:06:19 +0900

    purchaseのvalidatesの設定

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,11 @@
 class Purchase < ApplicationRecord
   attr_accessor :token
-  validates :token, presence: true
+  with_options presence: true do
+   validates :token
+   validates :phone_number
+   validates :building_name
+   validates :local_address
+   validates :local_position_town
+   validates :delivery_local_id, numericality: { other_than: 1, message: "can't be blank" }
+   validates :post_number
 end
